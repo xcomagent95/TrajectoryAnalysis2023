1e74a8a305e33729e41ae564192ab4670ad8c968
Soumya Ganguly <41335019+SoumyaGanguly95@users.noreply.github.com>
2023-07-21 21:07:52 +0200

    Implement trajectory segmentation (#5)
    * feat: Add Trajectory Segmentation with unit tests
    
    * chore: update README.md

--- project/test.py
+++ project/test.py
@@ -1,4 +1,5 @@
 # imports
+from utils import segmentTrajectory
 import datetime
 import math
 import unittest
@@ -9,7 +10,7 @@ import utils
 import functions_template as functions
 
 class DouglasPeuckerTest(unittest.TestCase):
-    def test1(self):#add better name
+    def test1(self):  # add better name
         traj_points = [[1, 1], [2, 2], [3, 3], [4, 4]]
         points = []
         for idx, p in enumerate(traj_points):
@@ -22,7 +23,7 @@ class DouglasPeuckerTest(unittest.TestCase):
             res_pojnts_array.append([p.X, p.Y])
         self.assertEqual(res_pojnts_array, [[1, 1], [4, 4]])
 
-    def test2(self):  #add better name
+    def test2(self):  # add better name
         traj_points = [
             [0.0, 0.0],
             [3.0, 8.0],
@@ -52,7 +53,7 @@ class DouglasPeuckerTest(unittest.TestCase):
                 7.0, 25.0], [11.0, 5.5], [27.8, 0.1]],
         )
 
-    def test3(self):#add better name
+    def test3(self):  # add better name
         traj_points = [
             (0.0, 0.0),
             (1.0, 1.0),
@@ -78,13 +79,13 @@ class DouglasPeuckerTest(unittest.TestCase):
             [[0.0, 0.0], [2.0, 2.0], [4.0, 0.0], [6.0, 2.0], [8.0, 0.0]]
         )
 
-    #Edgecase Testing
-    def test4(self):#add better name
+    # Edgecase Testing
+    def test4(self):  # add better name
         t = trajectory.trajectory(1, points=[])
         d = functions.douglasPeucker(t, 1)
         self.assertEqual(d, t)
 
-    def test5(self):#add better name
+    def test5(self):  # add better name
         traj_points = [
             (0.0, 0.0),
             (1.0, 1.0),
@@ -102,7 +103,7 @@ class DouglasPeuckerTest(unittest.TestCase):
         traj = trajectory.trajectory(1, points=points)
         self.assertRaises(ValueError, functions.douglasPeucker, traj, -1)
 
-    def test6(self):#add better name
+    def test6(self):  # add better name
         traj_points = [[1, 1], [2, 2], [3, 3], [4, 4]]
         points = []
         for idx, p in enumerate(traj_points):
@@ -111,7 +112,7 @@ class DouglasPeuckerTest(unittest.TestCase):
         d = functions.douglasPeucker(traj, 0)
         self.assertIsInstance(d, trajectory.trajectory)
 
-    def test7(self):#add better name
+    def test7(self):  # add better name
         traj_points = [[1, 1], [2, 2], [3, 3], [4, 4]]
         points = []
         for idx, p in enumerate(traj_points):
@@ -122,22 +123,19 @@ class DouglasPeuckerTest(unittest.TestCase):
             self.assertIsInstance(p, point.point)
 
 
-
-
-
 class DynamicTimeWarpingTest(unittest.TestCase):
     def test_dtw_distance(self):
         # Define the trajectory data
-        first_trajectory = [
+        first_trajectoryTest = [
             (0.0014788576577, 0.0037183030576),
             (0.0014788576577, 0.0037183030576),
             (0.0014788576577, 0.0037183030576),
             # Add more points if needed
         ]
         traj0 = trajectory.trajectory(1, points=[point.point(
-            p[0], p[1], idx) for idx, p in enumerate(first_trajectory)])
+            p[0], p[1], idx) for idx, p in enumerate(first_trajectoryTest)])
 
-        second_trajectory = [
+        second_trajectoryTest = [
             (0.0014788576577, 0.0037183030576),
             (0.0014788576577, 0.0037183030576),
             (0.0014788576577, 0.0037183030576),
@@ -145,11 +143,11 @@ class DynamicTimeWarpingTest(unittest.TestCase):
             # Add more points if needed
         ]
         traj1 = trajectory.trajectory(1, points=[point.point(
-            p[0], p[1], idx) for idx, p in enumerate(second_trajectory)])
+            p[0], p[1], idx) for idx, p in enumerate(second_trajectoryTest)])
 
         # Calculate the expected distance
         expected_distance = sum(math.sqrt((p0[0] - p1[0]) ** 2 + (p0[1] - p1[1]) ** 2)
-                                for p0, p1 in zip(first_trajectory, second_trajectory))
+                                for p0, p1 in zip(first_trajectoryTest, second_trajectoryTest))
 
         # Call the dynamicTimeWarping function
         actual_distance = functions.dynamicTimeWarping(traj0, traj1)
@@ -161,7 +159,7 @@ class DynamicTimeWarpingTest(unittest.TestCase):
 # TODo:
 # check if result is correct
 # check correct epsilon input (0<, 1?)
-# 
+#
 class SlidingWindowTest(unittest.TestCase):
 
     def test_length0(self):   # fails as traj with 0 points is not created
@@ -171,7 +169,7 @@ class SlidingWindowTest(unittest.TestCase):
         self.assertEqual(functions.slidingWindow(traj, epsilon).points, [])
 
     def test_length1(self):
-        #build trajectory
+        # build trajectory
         traj_points = [[1, 1]]
         points = []
         for idx, p in enumerate(traj_points):
@@ -181,7 +179,7 @@ class SlidingWindowTest(unittest.TestCase):
         self.assertEqual(functions.slidingWindow(traj, epsilon), traj)
 
     def test_length2(self):
-        #build trajectory
+        # build trajectory
         traj_points = [[1, 1], [2, 2]]
         points = []
         for idx, p in enumerate(traj_points):
@@ -189,8 +187,8 @@ class SlidingWindowTest(unittest.TestCase):
         traj = trajectory.trajectory(1, points=points)
         epsilon = 1
         self.assertEqual(functions.slidingWindow(traj, epsilon), traj)
-        #traj = [[0,0],[1,1]] 
-        #epsilon = 1
+        # traj = [[0,0],[1,1]]
+        # epsilon = 1
         self.assertEqual(functions.slidingWindow(traj, epsilon), traj)
 
     def test_epsilonNegativ(self):
@@ -204,7 +202,7 @@ class SlidingWindowTest(unittest.TestCase):
         traj = listOfTrajectories[0]
         epsilon = 0
         self.assertRaises(ValueError, functions.slidingWindow, traj, epsilon)
-    
+
     def test_ifCorrect(self):
         points = [
             (0.0014788576577, 0.0037183030576),
@@ -223,11 +221,11 @@ class SlidingWindowTest(unittest.TestCase):
         ]
         controlTraj = trajectory.trajectory(1, points=[point.point(
             p[0], p[1], idx) for idx, p in enumerate(controlPoints)])
-        
         epsilon = 0
         
         self.assertEqual(functions.slidingWindow(traj, epsilon), controlTraj)
 
+        self.assertEqual(functions.slidingWindow(traj, epsilon), controlTraj)
 
 
 class solveQueryWithoutRTree(unittest.TestCase):
@@ -255,6 +253,44 @@ class solveQueryWithoutRTree(unittest.TestCase):
         listOfTrajectories = utils.importTrajectories("Trajectories")
         queryRegion = region.region(point.point(0.0012601754558545508, 0.0027251228043638775, 0.0), -1)
         self.assertRaises(ValueError, functions.solveQueryWithoutRTree, queryRegion, listOfTrajectories)
-            
+
+
+class TestSegmentTrajectory(unittest.TestCase):
+
+    def segment_trajectory_single_segmentTest(self):
+        # Test case with a single segment
+        trajectory_input = [
+            point(10, 20, "2000-01-01:01:14:56"),
+            point(15, 25, "2000-01-01:01:15:52"),
+            point(18, 22, "2000-01-01:01:16:56"),
+            point(12, 28, "2000-01-01:01:19:01"),
+            point(8, 24, "2000-01-01:01:19:06")
+        ]
+        time_threshold_in_minutes = 5
+
+        segmented_trajectory = segmentTrajectory(
+            trajectory_input, time_threshold_in_minutes)
+
+        self.assertEqual(len(segmented_trajectory), 1)
+        self.assertEqual(len(segmented_trajectory[0]), len(trajectory_input))
+
+    def segment_trajectory_multiple_segmentsTest(self):
+        # Test case with multiple segments
+        trajectory_input = [
+            point(10, 20, "2000-01-01:01:14:56"),
+            point(15, 25, "2000-01-01:01:15:52"),
+            point(18, 22, "2000-01-01:01:16:56"),
+            point(12, 28, "2000-01-01:01:19:01"),
+            point(8, 24, "2000-01-01:01:19:06")
+        ]
+        time_threshold_in_minutes = 2
+
+        segmented_trajectory = segmentTrajectory(
+            trajectory_input, time_threshold_in_minutes)
+
+        self.assertEqual(len(segmented_trajectory), 2)
+        self.assertEqual(len(segmented_trajectory[0]), 3)
+        self.assertEqual(len(segmented_trajectory[1]), 2)
+
 if __name__ == "__main__":
     unittest.main()
